301a7c582da9477b518e4ebe4c30422df862762a
laurasturaine <laura.sturaine@edu.rtu.lv>
2023-02-19 13:44:55 +0200

    update1

--- main.py
+++ main.py
@@ -1,30 +1,43 @@
-# python3
-
-from collections import namedtuple
-
-Bracket = namedtuple("Bracket", ["char", "position"])
-
-
-def are_matching(left, right):
-    return (left + right) in ["()", "[]", "{}"]
-
-
-def find_mismatch(text):
-    opening_brackets_stack = []
-    for i, next in enumerate(text):
-        if next in "([{":
-            # Process opening bracket, write your code here
-            pass
-
-        if next in ")]}":
-            # Process closing bracket, write your code here
-            pass
-
-def main():
-    text = input()
-    mismatch = find_mismatch(text)
-    # Printing answer, write your code here
-
-
-if __name__ == "__main__":
+# python3
+
+from collections import namedtuple
+
+Bracket = namedtuple("Bracket", ["char", "position"])
+
+
+def are_matching(left, right):
+    return (left + right) in ["()", "[]", "{}"]
+
+
+def find_mismatch(text):
+    opening_brackets_stack = []
+    for i, next in enumerate(text):
+        if next in "([{":
+            # Process opening bracket, write your code here
+            opening_brackets_stack.append(Bracket(next, i))
+            pass
+
+        if next in ")]}":
+            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
+                return i+1
+                opening_brackets_stack.pop()
+            if opening_brackets_stack:
+                return opening_brackets_stack[0].position+1
+            
+            return "Success"
+            # Process closing bracket, write your code here
+            
+
+
+def main():
+    text = input()
+    mismatch = find_mismatch(text)
+    if mismatch=="Success":
+        print("Success")
+    else:
+    # Printing answer, write your code here
+        print(mismatch)
+
+
+if __name__ == "__main__":
     main()
