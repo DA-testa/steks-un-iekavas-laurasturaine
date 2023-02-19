8848cbb220055df0b1cad4c6383ea2787c463093
laurasturaine <laura.sturaine@edu.rtu.lv>
2023-02-19 13:57:13 +0200

    update4

--- main.py
+++ main.py
@@ -18,9 +18,16 @@ def find_mismatch(text):
             
 
         if next in ")]}":
-            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
+            if not opening_brackets_stack:
+                
+                return i+1
+
+            if not are_matching(opening_brackets_stack[-1].char, next):
+            
                 return i+1
-                opening_brackets_stack.pop()
+            
+            opening_brackets_stack.pop()
+            
             if opening_brackets_stack:
                 return opening_brackets_stack[0].position
             
